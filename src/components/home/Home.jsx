7d77ade9aad2f6a63b1fc6765c69c0fe2be6cf7a
hieudoann <https://github.com/hieudoann/FE-Mainternance>
2025-02-20 14:52:32 +0700

    update

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -118,7 +118,7 @@ function Home() {
   };
 
   const filteredData = data.filter((row) =>
-    row.id_thiet_bi.toLowerCase().includes(searchId.toLowerCase())
+    String(row.id_thiet_bi).toLowerCase().includes(searchId.toLowerCase())
   );
   const paginatedData = filteredData.slice((currentPage - 1) * ITEMS_PER_PAGE, currentPage * ITEMS_PER_PAGE);
   const totalPages = Math.ceil(filteredData.length / ITEMS_PER_PAGE);
